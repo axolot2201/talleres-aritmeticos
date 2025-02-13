c1ee09b046e2b2eb81b8f69ef7347122e4db7ec0
axolot2201 <georgepipe2005@gmail.com>
2025-02-13 08:42:21 -0500

    Create punto 1.py

---
+++ punto 1.py
@@ -0,0 +1,12 @@
+num1= int(input("ingrese un numero"))
+num2= int(input("ingrese otro numero"))
+suma= num1 + num2
+resta= num1 - num2
+multiplicacion= num1 * num2
+division= num1 / num2
+porcentaje= num2/100*num1
+print("La suma es: ", suma)
+print("La resta es: ", resta)
+print("La multiplicacion es: ", multiplicacion)
+print("La division es: ", division)
+print("el porcentaje es: ", porcentaje)
